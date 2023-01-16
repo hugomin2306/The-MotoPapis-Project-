5865bb54ac5a9d68d0edd6a5ba6132dd245e530c
ivan <fernandezbarcoivan@gmail.com>
2023-01-16 11:58:15 +0100

    Con la tabla link y nodos destroy

---
+++ historia/app/Http/Controllers/FinalesAlcanzadosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Finales_alcanzados;
+use Illuminate\Http\Request;
+
+class FinalesAlcanzadosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Finales_alcanzados  $finales_alcanzados
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Finales_alcanzados $finales_alcanzados)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Finales_alcanzados  $finales_alcanzados
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Finales_alcanzados $finales_alcanzados)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Finales_alcanzados  $finales_alcanzados
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Finales_alcanzados $finales_alcanzados)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Finales_alcanzados  $finales_alcanzados
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Finales_alcanzados $finales_alcanzados)
+    {
+        //
+    }
+}
