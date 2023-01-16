d6b34c4f2da20e5bf60789c33c9e7f67d44dc784
ivan <fernandezbarcoivan@gmail.com>
2023-01-16 21:34:46 +0100

    Hecho por Alexandru. Vista de edit y edit & update hechos

---
+++ historia/resources/views/nodos/edit.blade.php
@@ -0,0 +1,21 @@
+<x-zz.base>
+    <x-slot:titulo>Editar nodo</x-slot:titulo>
+
+    <form action='{{ route('nodos.update', $nodo) }}' method='post'>
+        @method('put')
+
+        <label for='id_nodo'>ID Nodo</label>
+        <input id='id_nodo' name='id_nodo' type='text' value='{{ $nodo->id_nodo }}'>
+        <br />
+
+        <label for='id_partida'>ID Partida</label>
+        <input id='id_partida' name='id_partida' type='text' value='{{ $nodo->id_partida }}'>
+        <br />
+
+        <label for='descripcion'>Descripcion</label>
+        <input id='descripcion' name='descripcion' type='text' value='{{ $nodo->descripcion_nodo }}'>
+        <br />
+
+        <button type='submit'>Actualizar</button>
+    </form>
+</x-zz.base>
