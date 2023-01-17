298c35241c1b68c8e51e6daf12b592859717c572
seroblaye <sblancoyebes@educa.madrid.org>
2023-01-17 09:31:58 +0100

    Componente base actualizado

--- historia/resources/views/componentes/base.blade.php
+++ historia/resources/views/componentes/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>{{ $tituloHead ?? 'Laragenda' }}</title>
+    <title>{{ $tituloHead ?? 'Historia Interactiva' }}</title>
 </head>
 <body>
     <x-zz.menu />
