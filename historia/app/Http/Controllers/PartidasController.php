51d306b5c963b328a01a8ee2aa24a99d41fed2ff
Alejandro Quiñones <theadrialexjo98@gmail.com>
2023-01-19 14:06:27 +0100

    Index de partidas + controller index

--- historia/app/Http/Controllers/PartidasController.php
+++ historia/app/Http/Controllers/PartidasController.php
@@ -7,79 +7,43 @@ use Illuminate\Http\Request;
 
 class PartidasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        $partida = Partidas::orderBy('id')->get();
+        return view('paginas/partidas/index', compact('partida'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Partidas  $partidas
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Partidas $partidas)
     {
-        //
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Partidas  $partidas
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Partidas $partidas)
     {
-        //
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Partidas  $partidas
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Partidas $partidas)
     {
-        //
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Partidas  $partidas
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Partidas $partidas)
     {
-        //
+
     }
 }
