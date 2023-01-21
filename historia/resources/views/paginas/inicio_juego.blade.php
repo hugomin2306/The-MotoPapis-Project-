2971f2299accc59cd03e894c959f50852dd25a5a
ivan <fernandezbarcoivan@gmail.com>
2023-01-21 20:03:36 +0100

    Inicio del juego version 0.2

--- historia/resources/views/paginas/inicio_juego.blade.php
+++ historia/resources/views/paginas/inicio_juego.blade.php
@@ -2,9 +2,24 @@
 
     <x-slot:tituloHead>Inicio Juego</x-slot:tituloHead>
     <x-slot:titulo>Historia Interactiva</x-slot:titulo>
+    <style>
+        h1{
+            text-align: center;
+        }
+        table{
+            display: flex;
+            justify-content: center;
+            text-align: center;
+        }
+    </style>
 
-    <a href='{{ route('jugadores.create') }}'>Nueva partida</a>
-    <br />
-    <a href='{{ route('partidas.index') }}'>Cargar partida</a>
+    <table>
+        <tr>
+            <th><a href='{{ route('jugadores.create') }}'>Nueva partida</a></th>
+        </tr>
+        <tr>
+            <th><a href='{{ route('partidas.index') }}'>Cargar partida</a></th>
+        </tr>
+    </table>
 
 </x-zz.base>
