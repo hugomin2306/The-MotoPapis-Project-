716b42a90cb667bc9e83a6b0a3c8dae9dc5c6425
Alejandro Quiñones <theadrialexjo98@gmail.com>
2023-01-27 12:08:15 +0100

    Actualizado

---
+++ historia/resources/views/paginas/jugadores/inicio.blade.php
@@ -0,0 +1,26 @@
+<x-zz.base_inicio>
+
+    <x-slot:tituloHead>Historia Interactiva</x-slot:tituloHead>
+    <x-slot:titulo>Iniciar Sesion</x-slot:titulo>
+
+    <form action='{{ route('jugadores.store') }}' method='post'>
+        @method('post')
+        @csrf
+
+        <label for='nombre_jugador'>Nombre de ususario: </label>
+        <input id='nombre_jugador' name='nombre_jugador' type='text'>
+        <br />
+        <br />
+
+        <label for='clave_jugador'>Contraseña: </label>
+        <input id='clave_jugador' name='clave_jugador' type='password'>
+        <br />
+        <br />
+
+        <input class='boton' type='submit' name='crear' value='Iniciar Sesion'>
+        <br />
+        <br />
+    </form>
+
+</x-zz.base_inicio>
+
