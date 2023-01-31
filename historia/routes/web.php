71036208e74795b5444af55e349a819a3f3b8eba
ivan <fernandezbarcoivan@gmail.com>
2023-01-31 09:43:31 +0100

    Web.php con las rutas del incio de sesion

--- historia/routes/web.php
+++ historia/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FinalesController;
 use App\Http\Controllers\JugadoresController;
@@ -16,25 +16,22 @@ Route::resource('nodos', NodosController::class);
 Route::resource('links', LinksController::class);
 Route::resource('partidas', PartidasController::class);
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('/zzz/welcome');
-});
-
-Route::get('/inicio', function() {
-    return view('/paginas/jugadores/create');
-});
-
 Route::get('/juego', function() {
     return view('/paginas/juegos/juego');
 });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
