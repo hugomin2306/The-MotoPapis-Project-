4057b7f3795c6eae62b26c973bbdcc7533bf558d
Alejandro Quiñones <theadrialexjo98@gmail.com>
2023-02-03 11:53:27 +0100

    Entrega 03/02/2023.

--- historia/resources/views/components/zz/menu.blade.php
+++ historia/resources/views/components/zz/menu.blade.php
@@ -1,5 +1,6 @@
 <nav id="menu">
     <div id="contenidoMenu">
+        <a class="elementoMenu" href='{{ route('juego') }}'>       Partidas</a>
         <a class="elementoMenu" href='{{ route('nodos.index')  }}'>Páginas</a>
         <a class="elementoMenu" href='{{ route('nodos.create') }}'>Crear&nbsp;página</a>
         <a class="elementoMenu" href='{{ route('links.index')  }}'>Decisiones</a>
