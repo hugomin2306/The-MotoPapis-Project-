d4efa4559d83ead17421424827a630b054d9d6eb
seroblaye <sblancoyebes@educa.madrid.org>
2023-02-07 12:29:08 +0100

    Login con redireccion a registrar + Log Out

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FinalesController;
+use App\Http\Controllers\JugadoresController;
+use App\Http\Controllers\NodosController;
+use App\Http\Controllers\LinksController;
+use App\Http\Controllers\PartidasController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+
+
+Route::resource('finales', FinalesController::class);
+Route::resource('jugadores', JugadoresController::class);
+Route::resource('nodos', NodosController::class);
+Route::resource('links', LinksController::class);
+Route::resource('partidas', PartidasController::class);
+
+Route::get('/juego', function() {
+    return view('/paginas/juegos/juego');
+})->name('juego');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+
+Route::get('/juego', function () {
+    return view('/paginas/juegos/juego');
+})->middleware(['auth', 'verified'])->name('juego');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
